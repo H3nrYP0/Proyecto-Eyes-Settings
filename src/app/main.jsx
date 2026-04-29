e8997560ddfacce43d887f9bb19c106c0680c191
Andres-H-Santana <andresfelipesantana53@gmail.com>
2026-04-29 16:57:09 -0500

    Agregacion de la cofig de reactQuery

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider, CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from './App.jsx'
 import './App.css'
 import theme from "../theme/theme.js";
 
+// se crea el cliente
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
