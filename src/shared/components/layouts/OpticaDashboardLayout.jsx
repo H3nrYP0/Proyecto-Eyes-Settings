bbd332fe865c7dc0921b246e8a245899719fc2f9
Andres-H-Santana <andresfelipesantana53@gmail.com>
2026-05-22 12:10:57 -0500

    cambio para hacer logout

--- src/shared/components/layouts/OpticaDashboardLayout.jsx
+++ src/shared/components/layouts/OpticaDashboardLayout.jsx
@@ -93,6 +93,7 @@ import DetalleUsuario from "@seguridad/user/pages/DetalleUsuario";
 
 // ========== Configuración ==========
 import Configuracion from "@configuracion/pages/Configuration";
+import authServices  from "@auth/services/authServices";
 
 export default function OpticaDashboardLayout({ user, setUser }) {
   const [sidebarOpen, setSidebarOpen] = useState(true);
